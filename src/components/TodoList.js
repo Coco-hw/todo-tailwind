549c80c7d70b478d97c5bbfa3675f66f16d7f191
Coco-hw <81812322+Coco-hw@users.noreply.github.com>
2023-04-18 12:31:15 +0900

    ring

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -91,7 +91,7 @@ const TodoList = () => {
           //   background-color: #fff;
           //   color: #0070f3;
           // }
-          className="rounded-full w-40 justify-self-end p-1 mb-4 ring-offset-2 ring-indigo-500 text-indigo-500 hover:bg-indigo-500 hover:text-white"
+          className="rounded-full w-40 justify-self-end p-1 mb-4 ring ring-offset-2 ring-indigo-500 text-indigo-500 hover:bg-indigo-500 hover:text-white"
           onClick={addTodo}
         >
           Add Todo
